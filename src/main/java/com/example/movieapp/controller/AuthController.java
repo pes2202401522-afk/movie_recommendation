269ca71051f5269d59ec4aa04f8982bbe2c2d1a2
Padmesh <padmesh19@gmail.com>
2026-04-08 01:12:57 +0530

    Module 1 completed: User Auth, H2 DB, Netflix UI, Movie CRUD

---
+++ src/main/java/com/example/movieapp/controller/AuthController.java
@@ -0,0 +1,52 @@
+package com.example.movieapp.controller;
+
+import com.example.movieapp.model.User;
+import com.example.movieapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/register")
+    public String showRegister(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute User user) {
+        userService.registerUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String showLogin() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String email,
+                        @RequestParam String password,
+                        Model model) {
+
+        User user = userService.login(email, password);
+
+        if (user != null) {
+            return "redirect:/movies"; // future module
+        } else {
+            model.addAttribute("error", "Invalid credentials");
+            return "login";
+        }
+    }
+}
---
+++ src/main/java/com/example/movieapp/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.movieapp.service;
+
+import com.example.movieapp.model.User;
+import com.example.movieapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User login(String email, String password) {
+        User user = userRepository.findByEmail(email);
+
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        return null;
+    }
+}
